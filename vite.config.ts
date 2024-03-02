9807cb478b5722df068f98f044fd44b1393f6db4
Renan Gimenez <renangiimenez@gmail.com>
2024-03-02 15:52:13 -0300

    feat: implement pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "POC React Router DOM",
+        short_name: "POC React Router",
+        description: "Testing React Router DOM",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "vite.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
+});
